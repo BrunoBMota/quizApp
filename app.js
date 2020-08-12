a2df5ccc6d36c1b90f32d44a6767c60ae60f2fdb
Bruno Boscardin Mota <bruno.boscardinmota@knights.ucf.edu>
2020-08-12 12:58:27 -0400

    added html and js

--- app.js
+++ app.js
@@ -2,35 +2,65 @@
  * Example store structure
  */
 'use strict'
-const store = {
+let lastAnswer = false;
+let questions = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: ' What was the name of the halo ring that Master Chief destroys at the end of Halo 3?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'Installation 00',
+        'The Ark',
+        'Installation 08',
+        'Installation 04'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'c'
     },
     {
-      question: 'What is the current year?',
+      question: 'What city did the Locust attack occur in at the start of Gears of War 2?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Nexus',
+        'Ilima',
+        'Tyrus',
+        'Jacinto City'
       ],
-      correctAnswer: '2019'
-    }
+      correctAnswer: 'd'
+    },
+    {
+      question: 'What was the name of the town that Mario had to clean up while on vacation?',
+      answers: [
+        'Mushroom Kingdom',
+        'Delfino Island',
+        'Toad Town',
+        'Cheep-Cheep Island',
+      ],
+      correctAnswer: 'b'
+    },
+    {
+      question: 'What is Sonic the Hedgehog\'s nemesis name?',
+      answers: [
+        'Knuckles',
+        'Shadow',
+        'Rouge',
+        'Dr. Eggman',
+      ],
+      correctAnswer: 'd'
+    },
+    {
+      question: 'Where is Link from?',
+      answers: [
+        'The Lost Forest',
+        'Kakariko Forest',
+        'Hyrule',
+        'Gerudo Valley',
+      ],
+      correctAnswer: 'b'
+    },
   ],
   quizStarted: false,
   questionNumber: 0,
   score: 0
 };
-
 /**
  * 
  * Technical requirements:
@@ -47,13 +77,134 @@ const store = {
  */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
-
 // These functions return HTML templates
+function introView(){
+  return `
+  <div class="introView">
+      <h1>Video Game Trivia!</h1>
+      <h2>Score 3/5 to win!</h2>
+      <button>Start</button>
+    </div>
+  `;
+}
+
+function questionView(){
+  let question = questions.questions[questions['questionNumber']];
+  return `
+  <div class="questionView">
+      <h1>Question ${questions['questionNumber'] + 1} of 5</h1>
+      <div class="questionContainer">
+        <p>${question['question']}</p>
+        <form>
+          <input type="radio" name="selection" id="a" value="a">
+          <label for="">A. ${question['answers'][0]}</label>
+          <br>
+          <input type="radio" name="selection" id="b" value="b">
+          <label for="">B. ${question['answers'][1]}</label>
+          <br>
+          <input type="radio" name="selection" id="c" value="c">
+          <label for="">C. ${question['answers'][2]}</label>
+          <br>
+          <input type="radio" name="selection" id="d" value="d">
+          <label for="">D. ${question['answers'][3]}</label>
+          <br>
+          ${questions['questionNumber'] === 4 ? `<input type="button" name="final-submit" id="final" value="SUBMIT FINAL RESULTS">` : `<input type="button" name="next-submit" id="next" value="SUBMIT">`}
+        </form>
+      </div>
+    </div>
+  `;
+}
+
+function feedbackView(){
+  return `
+  <div class="feedbackView">
+      <p>${lastAnswer ? 'correct': 'incorrect'}</p>
+      <p>{if incorrect will display the correct answer. else will just display good job}</p>
+      <button>Continue</button>
+    </div>
+  `;
+}
+
+
+function resultsView(){
+  return `
+  <div class="resultsView">
+      <div class="resultsContainer">
+        <h2>END OF GAME</h2>
+        <h3>Score: </h3>
+        <ul>
+          <li>
+            <p>Correct {#}</p>
+          </li>
+          <li>
+            <p>Incorret {#}</p>
+          </li>
+        </ul>
+      </div>
+      <p>{Results Message}</p>
+      <button>NEW GAME</button>
+    </div>
+  `;
+}
+
 
 /********** RENDER FUNCTION(S) **********/
-
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
+function renderIntroView(){
+  $('main').html(introView());
+}
+
+function renderQuestionView(){
+  $('main').html(questionView());
+}
+
+function renderFeedbackView(){
+  $('main').html(feedbackView());
+}
+
+function renderResultsView(){
+  $('main').html(resultsView());
+}
 
 /********** EVENT HANDLER FUNCTIONS **********/
-
 // These functions handle events (submit, click, etc)
+function submitAnswer(event){
+  event.preventDefault();
+  if(questions['questionNumber'] < 4){
+    if($('input[name=selection]:checked').val() === questions.questions[questions['questionNumber']].correctAnswer){
+      console.log('correct');
+      lastAnswer = true;
+      questions['questionNumber']++;
+      questions['score']++;
+    }
+    else{
+      console.log('false');
+      lastAnswer = false;
+      questions['questionNumber']++;
+    }
+    renderFeedbackView();
+  }
+
+  if(questions['questionNumber'] === 4){
+    if($('input[name=selection]:checked').val() === questions.questions[questions['questionNumber']].correctAnswer){
+      console.log('correct');
+      lastAnswer = true;
+      questions['score']++;
+    }
+    else{
+      console.log('false');
+      lastAnswer = false;
+    }
+  }
+}
+
+function main(){
+  renderIntroView();
+}
+
+$('main').on('click', '#next', submitAnswer);
+$('main').on('click', 'button', renderQuestionView);
+$('main').on('click', '#final', renderResultsView);
+
+
+$(main);
